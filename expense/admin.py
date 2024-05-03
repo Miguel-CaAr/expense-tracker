95cf47ffb2a7b775557eea1ab105b9fa61beb736
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-05-02 23:28:06 -0700

    - Models - Registration in admin for models - Time zone changed

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import expense, category
 # Register your models here.
+
+@admin.register(category)
+class expenseAdmin(admin.ModelAdmin):
+  fields = ['user_id', 'name', 'description', 'date_of_creation']
+  list_display = ['user_id', 'name', 'description', 'date_of_creation']
+  readonly_fields = ("date_of_creation",)
+  
+@admin.register(expense)
+class categoryAdmin(admin.ModelAdmin):
+  fields = ['user_id', 'amount', 'description', 'category_id', 'date_of_registration']
+  list_display = ['user_id', 'amount', 'description', 'category_id', 'date_of_registration']
+  readonly_fields = ("date_of_registration",)
--- expense/models.py
+++ expense/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class category(models.Model):
+    user_id = models.ForeignKey(
+        User, verbose_name="ID del usuario", on_delete=models.CASCADE)
+    name = models.CharField(
+        verbose_name="Nombre de la categoria", max_length=100)
+    description = models.TextField(
+        verbose_name="Descripcion de la categoria", max_length=500)
+    date_of_creation = models.DateTimeField(
+        verbose_name="Fecha de creaicon", auto_now_add=True)
+
+    class Meta:
+        db_table = "Categoria"
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self) -> str:
+        return f"{self.name} - by {self.user_id.username}"
+
+
+class expense(models.Model):
+    user_id = models.ForeignKey(
+        User, verbose_name="ID del usuario", on_delete=models.CASCADE)
+    amount = models.DecimalField(
+        verbose_name="Monto del gasto", max_digits=8, decimal_places=2)
+    description = models.TextField(
+        verbose_name="Descripcion del monto", blank=True, max_length=500)
+    category_id = models.ForeignKey(
+        category, verbose_name="Categoria del gasto", on_delete=models.SET_NULL, null=True)
+    date_of_registration = models.DateTimeField(
+        verbose_name="Fecha del registro", auto_now_add=True)
+
+    class Meta:
+        db_table = "Gastos"
+        verbose_name = "Gasto"
+        verbose_name_plural = "Gastos"
+
+    def __str__(self) -> str:
+        return f"{self.description} - by {self.user_id.username}"
